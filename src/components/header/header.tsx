05f0afea6caedb4ccdd2e794e27ba78359af8cb5
YolhaYu <olenka_glushko1993@mail.ru>
2023-03-25 13:38:32 +0300

    fix: fix title in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,42 +1,28 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './header.css';
 import { PageState, Props } from '../../types';
 
 class Header extends React.Component<Props, PageState> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      page: localStorage.getItem('page') || 'Home',
-    };
-    this.changeValue = this.changeValue.bind(this);
-  }
-
-  changeValue(e: React.MouseEvent) {
-    const { page } = this.state;
-    let value = '';
-    if (e.currentTarget.textContent) {
-      value = e.currentTarget.textContent;
-    }
-    this.setState(() => ({
-      page: value || page,
-    }));
-    localStorage.setItem('page', value);
-  }
-
   render(): React.ReactNode {
-    const { page } = this.state;
     return (
       <header className="header">
-        <h2>{page}</h2>
+        <h2 className="header__h2">
+          <Routes>
+            <Route path="/" element="Home" />
+            <Route path="/about" element="About" />
+            <Route path="/form" element="Form" />
+            <Route path="*" element="Not Found" />
+          </Routes>
+        </h2>
         <nav className="wrap-links">
-          <NavLink onClick={this.changeValue} className="link" to="/">
+          <NavLink className="link" to="/">
             Home
           </NavLink>
-          <NavLink onClick={this.changeValue} className="link" to="/about">
+          <NavLink className="link" to="/about">
             About
           </NavLink>
-          <NavLink onClick={this.changeValue} className="link" to="/form">
+          <NavLink className="link" to="/form">
             Form
           </NavLink>
         </nav>
